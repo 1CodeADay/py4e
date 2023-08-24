8169e34b3345aec06b1c4f7ee52b1e94bbff04b2
1day0C <oneday.1code@gmail.com>
2023-08-24 08:35:20 +0100

    Feat: loops and iteration
    - None type
    - Indefinite loop: while, break, continue
    - Definite loop: for, in
    - Counting in a loop
    - Finding the average in a loop
    - Finding the smallest, largest value
    - Filtering in a loop
    - None type : to be used on boolean and Note type
    Iteration variables
    Loop idioms
    
    An input always return a string value

---
+++ ex_05/ex_05_02.py
@@ -0,0 +1,65 @@
+#Exercise 2: Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'.
+# Once 'done' is entered, print out the largest and smallest of the numbers.
+# If the user enters anything other than a valid number catch it with a try/except and
+# put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
+
+# smallest = None
+# largest = None
+# count = 0
+
+# print('Before')
+# while True:
+#     sval = input('Enter a number: ')
+#     if sval == 'done' :
+#         break
+   
+#     try:
+#         fval = float(sval)
+        
+#         if smallest is None :
+#             smallest = fval
+#         elif fval < smallest :
+#             smallest = fval
+#         elif largest is None :
+#             largest = fval
+#         elif fval > largest :
+#             largest = fval
+#     except:
+        
+#         print('Enter a valid number')
+#         continue
+#     count = count + 1
+        
+# print('Minimum is', smallest)
+# print('Maximum is', largest)
+# print(count)
+
+
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        fnum = float(num)
+        
+        if smallest is None:
+            smallest = fnum
+        elif fnum < smallest:
+            smallest = fnum
+        elif largest is None:
+            largest = fnum
+        elif fnum > largest:
+            largest = fnum
+    except:
+        print("Invalid input")
+        continue
+
+print("Maximum is", int(largest))
+print("Minimum is", smallest)
+    
+    
+    
+  
---
+++ ex_05/test.py
@@ -0,0 +1,43 @@
+# This is an infinite loop
+# n = 10
+# while True:
+#     print(n, end=' ')
+#     n = n - 1
+# print('Done!')
+
+
+
+# n = 0
+# while n > 0 :
+#     print('Lather')
+#     print('Rinse')
+# print('Dry off!')
+
+
+# smallest_so_far = -1
+# for the_num in [9, 41, 12, 3, 74, 15] :
+#    if the_num < smallest_so_far :
+#       smallest_so_far = the_num
+# print(smallest_so_far)
+
+
+
+# friends = ['Joseph', 'Glenn', 'Sally']
+# for friend in friends :
+#      print('Happy New Year:',  friend)
+# print('Done!')
+
+
+# tot = 0
+# for i in [5, 4, 3, 2, 1] :
+#     tot = tot + 1
+# print(tot)
+
+
+# ................................................
+# What is wrong with this Python loop: forever loop
+n = 5
+while n > 0 :
+    print(n)
+print('All done')
+
