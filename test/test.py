dd591d94a2fad31d0069cb8ac4556babc090fa83
1CodeADay <onecodeaday.ocad@gmail.com>
2023-09-07 19:55:23 +0100

    Feat : lesson on strings
    Length of a string using len
    Traversal through a string with a loop
    String slices s = 'Monty Python'>>> print(s[0:5])
    If you omit the first index (before the colon), the slice starts at the beginning of the string.
    If you omit the second index, the slice goes to the end of the string:
    String comparison
    String methods dir(stuff)
    Parsing strings
    Format operator (for now, this is a bit unclear to me)

---
+++ test/test.py
@@ -0,0 +1,20 @@
+# x = "banana"
+# print(len(x))
+
+# var = "banana"
+# index = 0
+# while index < len(var):
+#     letter = var[index]
+#     print(index, letter)
+#     index = index + 1
+    
+
+# var = 'banana'
+# for letter in var:
+#     print(letter)
+    
+
+greet = 'Hello Bob'
+var = greet.casefold()
+print(var)
+print(greet)
